4d39108fed9ad1829606c10be54947863402c0ac
Will Yager <will.yager@gmail.com>
2015-10-22 19:32:19 -0500

    Experimenting with type-tagging values with their originating stage

---
+++ CPU/Safety/Stages.hs
@@ -0,0 +1,30 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Stages (Stage(..), Succ, Pred, IsStage) where
+
+import CLaSH.Prelude
+
+data Stage = F | D | R | X
+
+type family Succ (s :: Stage) :: Stage where
+    Succ F = D
+    Succ D = R
+    Succ R = X
+
+type family Pred (s :: Stage) :: Stage where
+    Pred X = R
+    Pred R = D
+    Pred D = F
+
+class IsStage (stage :: Stage) where
+    rep :: Rep stage
+
+data Rep (a :: Stage) = Rep String
+
+instance Show (Rep a) where
+    show (Rep s) = s
+
+instance IsStage F where rep = Rep "F"
+instance IsStage D where rep = Rep "D"
+instance IsStage R where rep = Rep "R"
+instance IsStage X where rep = Rep "X"
